d9d7df393310adf49c30fc7aeef5fc4977c468f2
Bharadwaj Gadiyaram <bharat.gadiyaram@gmail.com>
2023-04-15 16:15:21 -0400

    Added args.h if statement in rps-cli.js

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -4,3 +4,17 @@ import { rps } from "../lib/rpsls.js"
 import minimist from 'minimist';
 
 var argv = minimist(process.argv.slice(2));
+
+if (args.h) {
+    Console.log("Usage: node-rps [SHOT]");
+    console.log("Play Rock Paper Scissors (RPS)")
+    console.log();
+    console.log(" -h, --help      display this help message and exit");
+    console.log(" -r, --rules     display the rules and exit");
+    console.log();
+    console.log("Examples:");
+    console.log("node-rps        Return JSON with single player RPS result.");
+    console.log("                e.g. { \"player\": \"rock\" }");
+    console.log("node-rps rock   Return JSON with results for RPS played against a simulated opponent.");
+    console.log("                  e.g {\"player\":\"rock\",\"opponent\":\"scissors\",\"result\":\"win\"}");
+}
--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -3,4 +3,4 @@
 import { rps } from "../lib/rpsls.js"
 import minimist from 'minimist';
 
-var argv = minimist(process.argv.slice(2));
+var args = minimist(process.argv.slice(2));
