d0a60261dc74cb89352384d6ab95f285a1310b23
Bharadwaj Gadiyaram <bharat.gadiyaram@gmail.com>
2023-04-15 17:42:49 -0400

    Forgot try catch

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -21,7 +21,7 @@ if (args.h || args.help) {
 
 if (args.r || args.rules) {
     console.log(`Rules for Rock Paper Scissors:
-    
+
         - Scissors CUTS Paper
         - Paper COVERS Rock
         - Rock CRUSHES Scissors`);
@@ -29,4 +29,24 @@ if (args.r || args.rules) {
 
 let player_shot = args._[0];
 
-console.log(rps(player_shot));
+try {
+    console.log(rps(player_shot));
+}
+catch (e) {
+    console.log(`Usage: node-rps [SHOT]
+        Play Rock Paper Scissors (RPS)
+        
+          -h, --help      display this help message and exit
+          -r, --rules     display the rules and exit
+        
+        Examples:
+          node-rps        Return JSON with single player RPS result.
+                          e.g. {"player":"rock"}
+          node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+                          e.g {"player":"rock","opponent":"scissors","result":"win"}`);
+    console.log(`Rules for Rock Paper Scissors:
+
+        - Scissors CUTS Paper
+        - Paper COVERS Rock
+        - Rock CRUSHES Scissors`);
+}
