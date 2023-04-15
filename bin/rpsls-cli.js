6114299b621c706364f714a4c347dc5431ffd051
Bharadwaj Gadiyaram <bharat.gadiyaram@gmail.com>
2023-04-15 17:31:43 -0400

    Rules for RPSLS

--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -18,3 +18,18 @@ if (args.h) {
     console.log("node-rps rock   Return JSON with results for RPS played against a simulated opponent.");
     console.log("                  e.g {\"player\":\"rock\",\"opponent\":\"scissors\",\"result\":\"win\"}");
 }
+
+if (args.r) {
+    console.log("Rules for Rock Paper Scissors:");
+    console.log();
+    console.log("  - Scissors CUTS Paper");
+    console.log("  - Paper COVERS Rock");
+    console.log("  - Rock SMOOSHES Lizard");
+    console.log("  - Lizard POISONS Spock");
+    console.log("  - Spock SMASHES Scissors");
+    console.log("  - Scissors DECAPITATES Lizard");
+    console.log("  - Lizard EATS Paper");
+    console.log("  - Paper DISPROVES Spock");
+    console.log("  - Spock VAPORIZES Rock");
+    console.log("  - Rock CRUSHES Scissors");
+}
