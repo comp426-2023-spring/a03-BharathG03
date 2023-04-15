23139b9ac15453e38c338ff40e32e1803467eeed
Bharadwaj Gadiyaram <bharat.gadiyaram@gmail.com>
2023-04-15 17:21:36 -0400

    Fixed typo in args.h rps-cli.js

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -14,7 +14,7 @@ if (args.h) {
     console.log();
     console.log("Examples:");
     console.log("node-rps        Return JSON with single player RPS result.");
-    console.log("                e.g. { \"player\": \"rock\" }");
+    console.log("                e.g. { \"player\": \"rock\"}");
     console.log("node-rps rock   Return JSON with results for RPS played against a simulated opponent.");
     console.log("                  e.g {\"player\":\"rock\",\"opponent\":\"scissors\",\"result\":\"win\"}");
 }
