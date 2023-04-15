928e0903068b0f5b377a1d185052d71c9c8998ff
Bharadwaj Gadiyaram <bharat.gadiyaram@gmail.com>
2023-04-15 17:57:46 -0400

    changed help message in rpsls-cli.js

--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -7,16 +7,16 @@ var args = minimist(process.argv.slice(2));
 
 if (args.h || args.help) {
     console.log("Usage: node-rps [SHOT]");
-    console.log("Play Rock Paper Scissors (RPS)")
+    console.log("Play the Lizard - Spock Expansion of Rock Paper Scissors(RPSLS)!");
     console.log();
-    console.log(" -h, --help      display this help message and exit");
-    console.log(" -r, --rules     display the rules and exit");
+    console.log("  - h, --help        display this help message and exit");
+    console.log("  - r, --rules       display the rules and exit");
     console.log();
     console.log("Examples:");
-    console.log("node-rps        Return JSON with single player RPS result.");
-    console.log("                e.g. { \"player\": \"rock\"}");
-    console.log("node-rps rock   Return JSON with results for RPS played against a simulated opponent.");
-    console.log("                  e.g {\"player\":\"rock\",\"opponent\":\"scissors\",\"result\":\"win\"}");
+    console.log("node-rps        Return JSON with single player RPSLS result.");
+    console.log("                e.g. { \"player\": \"rock\" }");
+    console.log("node-rps rock   Return JSON with results for RPSLS played against a simulated opponent.");
+    console.log("                e.g { \"player\": \"rock\", \"opponent\": \"Spock\", \"result\": \"lose\" }");
 }
 
 if (args.r || args.rules) {
