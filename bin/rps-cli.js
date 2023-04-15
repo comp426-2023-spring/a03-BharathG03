0065051dfcae781016fb3bed0a116184e369d90f
Bharadwaj Gadiyaram <bharat.gadiyaram@gmail.com>
2023-04-15 17:23:05 -0400

    added rules for rps-cli.js

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -3,7 +3,7 @@
 import { rps } from "../lib/rpsls.js"
 import minimist from 'minimist';
 
-var argv = minimist(process.argv.slice(2));
+var args = minimist(process.argv.slice(2));
 
 if (args.h) {
     Console.log("Usage: node-rps [SHOT]");
@@ -17,4 +17,11 @@ if (args.h) {
     console.log("                e.g. { \"player\": \"rock\"}");
     console.log("node-rps rock   Return JSON with results for RPS played against a simulated opponent.");
     console.log("                  e.g {\"player\":\"rock\",\"opponent\":\"scissors\",\"result\":\"win\"}");
+}
+
+if (args.r) {
+    console.log("Rules for Rock Paper Scissors:");
+    console.log(" - Scissors CUTS Paper");
+    console.log(" - Paper COVERS Rock");
+    console.log(" - Rock CRUSHES Scissors");
 }
